05c98fd7a920c7f8b964cbbe89c242ccd3e57bb8
xaviguasch <xaviguasch@gmail.com>
2019-04-30 13:11:26 +0200

    added shaking css property in sync with rolling state

--- src/Die.js
+++ src/Die.js
@@ -3,7 +3,7 @@ import './Die.css'
 
 class Die extends Component {
   render() {
-    return <i className={`Die fas fa-dice-${this.props.face}`} />
+    return <i className={`Die fas fa-dice-${this.props.face} ${this.props.rolling && 'shaking'}`} />
   }
 }
 
