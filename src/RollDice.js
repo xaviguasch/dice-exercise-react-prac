e900343c77cf87311b816f3ff007aa62d67c1a9b
xaviguasch <xaviguasch@gmail.com>
2019-04-30 12:49:28 +0200

    added rolling logic to button

--- src/RollDice.js
+++ src/RollDice.js
@@ -11,7 +11,8 @@ class RollDice extends Component {
     super(props)
     this.state = {
       die1: 'one',
-      die2: 'one'
+      die2: 'one',
+      rolling: false
     }
     this.roll = this.roll.bind(this)
   }
@@ -20,7 +21,11 @@ class RollDice extends Component {
     const newDie1 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
     let newDie2 = this.props.sides[Math.floor(Math.random() * this.props.sides.length)]
 
-    this.setState({ die1: newDie1, die2: newDie2 })
+    this.setState({ die1: newDie1, die2: newDie2, rolling: true })
+
+    setTimeout(() => {
+      this.setState({ rolling: false })
+    }, 1000)
   }
 
   render() {
@@ -30,7 +35,9 @@ class RollDice extends Component {
           <Die face={this.state.die1} />
           <Die face={this.state.die2} />
         </div>
-        <button onClick={this.roll}>Roll Dice!</button>
+        <button onClick={this.roll} disabled={this.state.rolling}>
+          {this.state.rolling ? 'Rolling' : 'Roll Dice!'}
+        </button>
       </div>
     )
   }
